988c6f4d2a14680d606a1792eaaea9e7c51359d3
Goutham-Karthik <Gouthaman_Karthikeyan@outlook.com>
2026-01-12 23:00:32 -0600

    Arrays - easy

--- src/DsaInOrder/Arrays/SummaryRanges.java
+++ src/DsaInOrder/Arrays/SummaryRanges.java
@@ -7,11 +7,15 @@ public class SummaryRanges {
         int[] nums = {0,1,2,4,5,7};
         String[] r1 = new String[nums.length];
         for(int i =0; i<nums.length; i++){
-            if( i + 1 < nums.length && nums[i]+1 == nums[i+1]) {
-                r1[i] = nums[i] + "->" + nums[i+1];
-            } else{
-                r1[i] = String.valueOf(nums[i]);
-            }
+           while(i < nums.length && nums[i] +1 == nums[i]){
+               i++;
+           }
+           if(nums[i] +1 != nums[i]){
+               r1[i] = r1[i] + "->" +nums[i];
+
+           } else {
+               r1[i] = String.valueOf(nums[i]);
+           }
         }
         System.out.println(Arrays.toString(r1));
     }
